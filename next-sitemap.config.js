64f726bd8bd484d8b81c2395f557bc1574d39628
JSC <gbs04087@gmail.com>
2022-10-18 06:32:27 +0900

    FEAT: next-sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://blog2-eosin.vercel.app',
+  siteUrl: 'https://my-first-programming.kr',
   generateRobotsTxt: true,
   sitemapSize: 7000,
 };
