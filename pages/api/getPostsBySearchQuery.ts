446a2de20690c6380271951032daa3ddcda5c98a
JSC <gbs04087@gmail.com>
2022-10-22 21:31:56 +0900

    FEAT: add api getPostsBySearchQuery

---
+++ pages/api/getPostsBySearchQuery.ts
@@ -0,0 +1,21 @@
+import { getAttributesOfContent } from '@common/frontMatter';
+import { getAllFiles } from 'common/fs';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+const getPostsBySearchQuery = (searchQuery: string) => {
+  const allFiles = getAllFiles();
+  const Regex = new RegExp(`${searchQuery}`);
+  return allFiles
+    .map((e) => getAttributesOfContent(e.content))
+    .filter((e) => {
+      return e.title.match(Regex) || e.description.match(Regex) || e.slug.match(Regex);
+    });
+};
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { searchQuery } = req.query;
+  if (req.method === 'GET' && typeof searchQuery === 'string') {
+    res.status(200).send(getPostsBySearchQuery(searchQuery));
+  } else {
+    res.status(400);
+  }
+}
